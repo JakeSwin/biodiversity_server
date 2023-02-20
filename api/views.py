dbf4dc2f4621b87b519abba6b10f6bbf2050ae58
JakeSwin <jacob.swindell@btinternet.com>
2023-02-20 16:59:31 +0000

    views

--- api/views.py
+++ api/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-# New tutorial
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 
 # Create your views here.
 from rest_framework import generics
@@ -15,11 +12,5 @@ class UserView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
-# @api_view(['POST'])
-# def UserPost(request):
-#     serializer = UserSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#     return Response(serializer.data)
     
 
